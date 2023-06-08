689391335e3cbdd44cfa724b7a0d66388000cb24
Sarsar Deli <deli@rushstreetinteractive.com>
2023-06-08 15:27:03 -0400

    Do not fail on missing fields

--- src/main/java/org/leanpoker/player/model/utils/GameParser.java
+++ src/main/java/org/leanpoker/player/model/utils/GameParser.java
@@ -2,12 +2,17 @@ package org.leanpoker.player.model.utils;
 
 import org.leanpoker.player.model.GameState;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public final class GameParser {
 
-    public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    public static final ObjectMapper OBJECT_MAPPER;
+
+    static {
+        OBJECT_MAPPER = new ObjectMapper();
+        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);    }
 
     public static GameState parse(JsonNode jsonNode) {
         return OBJECT_MAPPER.convertValue(jsonNode, GameState.class);

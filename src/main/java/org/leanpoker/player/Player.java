f75d5b87914bbe5939cd3194823192849fd6e568
Juan Almeida <almeida.j@gmail.com>
2023-06-08 10:08:40 -0400

    Modifying response to always call a bet

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,9 +7,16 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonNode request) {
-        return 100;
+        if (shouldFold()) {
+            return 0;
+        }
+        return request.get("minimum_raise");
     }
 
     public static void showdown(JsonNode game) {
+    }
+
+    private static boolean shouldFold() {
+        return false;
     }
 }

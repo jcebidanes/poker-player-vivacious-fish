a16de110ce7ef3bc831fdabe7e356b63daeb61ed
Juan Almeida <almeida.j@gmail.com>
2023-06-08 10:20:21 -0400

    1st logic to call/fold

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,16 +7,26 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonNode request) {
-        if (shouldFold()) {
+        int current_pot = Integer.valueOf(request.get("pot").asText());
+        int current_call = Integer.valueOf(request.get("current_buy_in").asText());
+
+        if (shouldFold(current_pot, current_call)) {
             return 0;
         }
-        return Integer.valueOf(request.get("current_buy_in").asText());
+        return current_call;
     }
 
     public static void showdown(JsonNode game) {
     }
 
-    private static boolean shouldFold() {
-        return false;
+    private static boolean shouldFold(int current_pot, int current_min_call) {
+        float winP = getWinProbability();
+        float gameCoef = winP*current_pot - (1 - winP)*(current_min_call);
+
+        return gameCoef <= 0;
+    }
+
+    private static float getWinProbability() {
+        return 1;
     }
 }
